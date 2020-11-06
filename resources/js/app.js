49fe90b367a0357a2d93001d06d9804edc528c57
Sean <s.frankruyter@gmail.com>
2020-11-06 14:04:48 -0500

    using ziggy to bind routes to vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,20 +5,22 @@ import Vue from 'vue';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import Route from '../../vendor/tightenco/ziggy/src/js/route.js';
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.prototype.$route = (...args) => Route(...args).url();
 
 const app = document.getElementById('app');
 
 new Vue({
-    render: (h) =>
+    render: h =>
         h(InertiaApp, {
             props: {
                 initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
-            },
-        }),
+                resolveComponent: name => require(`./Pages/${name}`).default
+            }
+        })
 }).$mount(app);
